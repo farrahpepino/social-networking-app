db315f23e24d5972a966bedb5ab56992282b39cd
farrahpepino <idcfarrah@gmail.com>
2025-08-31 23:47:51 -0400

    improved ui

--- AwsS3/obj/Debug/net9.0/AwsS3.AssemblyInfo.cs
+++ AwsS3/obj/Debug/net9.0/AwsS3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AwsS3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d0a20230eef504377af757e3b9bcdb8aaee67e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cecfbfda7477ba352606c3be3967dab22028c0f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AwsS3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AwsS3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d0a20230eef504377af757e3b9bcdb8aaee67e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cecfbfda7477ba352606c3be3967dab22028c0f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
