3fff9110a36b7b76f38e5274c2126352119b84a9
farrahpepino <idcfarrah@gmail.com>
2025-09-03 23:19:49 -0400

    updating sqls

--- server/Data/LoopDb.sql
+++ server/Data/LoopDb.sql
@@ -30,6 +30,7 @@ CREATE TABLE posts (
     AuthorId VARCHAR(36) NOT NULL,
     Content VARCHAR(1000) NOT NULL,
     ImageUrl VARCHAR(255),
+    ImageKey VARCHAR(255),
     CreatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (AuthorId) REFERENCES users(Id)
         ON DELETE CASCADE
@@ -62,26 +63,3 @@ CREATE TABLE likes(
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
-
-DELIMITER //
-
-CREATE PROCEDURE CreateInterest(
-    IN p_userId1 VARCHAR(36),
-    IN p_userId2 VARCHAR(36),
-    IN p_interestId VARCHAR(36),
-    IN p_createdAt TIMESTAMP
-)
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1 
-        FROM interests
-        WHERE UserId1 = p_userId1
-          AND UserId2 = p_userId2
-        LIMIT 1
-    ) THEN
-        INSERT INTO interests (Id, UserId1, UserId2, CreatedAt)
-        VALUES (p_interestId, p_userId1, p_userId2, p_createdAt);
-    END IF;
-END//
-
-DELIMITER ;
--- server/Data/Procedures.sql
+++ server/Data/Procedures.sql
@@ -1,23 +1,3 @@
-USE LoopDb;
-
-DELIMITER //
-
-CREATE PROCEDURE AddInterestToUser(
-    IN p_userId1 VARCHAR(36),
-    IN p_interestId VARCHAR(36)
-)
-BEGIN
-    UPDATE users
-    SET Interests = JSON_ARRAY_APPEND(
-        COALESCE(Interests, JSON_ARRAY()), '$', p_interestId
-    )
-    WHERE Id = p_userId1
-      AND JSON_CONTAINS(COALESCE(Interests, JSON_ARRAY()), JSON_QUOTE(p_interestId), '$') = 0
-    LIMIT 1;
-END//
-
-DELIMITER ;
-
 
 DELIMITER //
 
@@ -37,8 +17,6 @@ BEGIN
     ) THEN
         INSERT INTO interests (Id, UserId1, UserId2, CreatedAt)
         VALUES (p_interestId, p_userId1, p_userId2, p_createdAt);
-        
-        CALL AddInterestToUser(p_userId1, p_interestId);
     END IF;
 END//
 
