11960c6a54c9cf4b7d61b275f739ca21b048fdd5
farrahpepino <idcfarrah@gmail.com>
2025-09-13 14:13:31 -0400

    updates

--- AwsS3/obj/Debug/net9.0/AwsS3.AssemblyInfo.cs
+++ AwsS3/obj/Debug/net9.0/AwsS3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AwsS3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f46b1ce5f5f9afa6e9d64add83c3efbbbb81556")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96145dcc6d7db9a13743dc5c38632ccf57f823c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AwsS3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AwsS3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,7 +3,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http'; 
+import { provideServerRendering } from '@angular/platform-server';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideHttpClient(withFetch())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()), 
+    provideHttpClient(withFetch()), 
+    provideServerRendering()
+  ]
 };
